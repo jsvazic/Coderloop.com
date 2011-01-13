b4ac4d80e1f9725ac67da7dab77eef9c58e5c288
John Svazic <jsvazic@gmail.com>
2011-01-12 22:57:51 -0500

    Updates to get a slow, but functional, solution for euler 12.

--- euler12/euler12.clj
+++ euler12/euler12.clj
@@ -6,10 +6,13 @@
 (defn triangle-num [n]
   (/ (* n (+ n 1)) 2))
 
-; We're counting 1 and n already as factors, hence the reason we add 2 to the count.
 (defn num-factors [n]
   (+ 2 (count (for [x (range 2 (/ (+ n 1) 2)) :when (= 0 (mod n x))] [x]))))
 	
-(println (num-factors 100))
-  
-;(def my-pow (Integer/valueOf (process-file (first *command-line-args*))))
+(defn euler12 [n]
+  (loop [curr 1]
+	(if (< n (num-factors (triangle-num curr)))
+	  (triangle-num curr)
+	  (recur (inc curr)))))
+
+(println (euler12 (Integer/valueOf (process-file (first *command-line-args*)))))
