91f0d402e9e1a8b2b5c0381055fce77e8efe9da3
John Svazic <jsvazic@gmail.com>
2011-01-28 08:44:32 -0500

    Added a first attempt at breaking the input list into frames.

--- bowling/bowling.clj
+++ bowling/bowling.clj
@@ -1,32 +1,18 @@
-(defn bowling-sum [x y]
-  (cond
-    (= "X" x) (+ 10 (bowling-sum y nil))
-    (= "/" y) 10
-	(nil? x) 0
-	(nil? y) (Integer/valueOf x)
-	:else (+ (Integer/valueOf x) (Integer/valueOf y))))
-
-(defn get-frame-score [col]
-  (let [curr (first col)
-        first-ball (first (rest col))
-		second-ball (second (rest col))]		
+(defn create-frames [curr-frame col frames]
+  (let [ball-1 (first col)
+        ball-2 (second col)]
     (cond
-	  (= "X" curr) (+ 10 (bowling-sum first-ball second-ball))
-	  (= "/" curr) (+ 10 (bowling-sum first-ball nil))
-	  (= "/" first-ball) 0
-	  :else (Integer/valueOf curr))))
-
-(defn calc-score [col]
-  (cond
-    (empty? col) 0
-    :else (+ (get-frame-score col) (calc-score (rest col)))))
+	  (empty? col) frames
+	  (= 10 curr-frame) (recur curr-frame nil (cons frames col))
+	  (= "X" ball-1) (recur (inc curr-frame) (rest col) (cons frames ball-1))
+	  :else (recur (inc curr-frame) (doall (drop 2 col)) (cons frames (vector ball-1 ball-2))))))
 
 ;(def score ["X" "7" "/" "4" "5" "X" "9" "/" "8" "1" "3" "5" "X" "X" "3" "4"]) ; 145
 ;(def score ["7" "0" "4" "0" "6" "3" "9" "0" "8" "1" "3" "1" "3" "1" "8" "1" "2" "6" "0" "1"]) ; 64
 (def score ["0" "4" "5" "1" "X" "2" "4" "4" "3" "5" "0" "8" "/" "1" "2" "X" "6" "/" "4"]) ; 92
 ;(def score ["X" "X" "X" "X" "X" "X" "X" "X" "X" "X" "X" "X"]) ; 300
 
-(println (calc-score score))
+(println (create-frames 1 score ()))
 
 ; 1:  0 4    ==   4
 ; 2:  5 1    ==   6
